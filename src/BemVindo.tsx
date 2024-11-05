5b029303a09c6f940354dc8aca56bce7d0d21eee
Danilo Ponciano <danilo_ponciano@estudante.sc.senai@gmail.com>
2024-11-05 19:28:09 -0300

    Inicio de projeto react

---
+++ src/BemVindo.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function BemVindo(){
+    
+    return (
+    <>
+        <h1> Olá Mundo </h1>
+        <p>Aula de Front-end</p>
+    </>
+    )
+}
+
+export default BemVindo;
